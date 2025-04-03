f00d7ac7a3475ffc72dcb19fbd1f8ee4a96d657d
agamairi <agam.airi@gmail.com>
2025-04-02 23:04:02 -0400

    adding theme to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:job_swipe/screens/home_screen.dart';
+import 'package:job_swipe/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const HomeScreen());
+    return MaterialApp(home: const HomeScreen(), theme: buildAppTheme(context));
   }
 }
