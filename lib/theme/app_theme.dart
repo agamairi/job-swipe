b8c063e9b2718d713c7fe8aac01201dde6b96eb6
agamairi <agam.airi@gmail.com>
2025-04-05 11:58:20 -0400

    making app theme truly random

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 ThemeData buildAppTheme(BuildContext context) {
+  Color getRandomColor() {
+    Random random = Random();
+    return Color.fromRGBO(
+      random.nextInt(256), // Random red value (0-255)
+      random.nextInt(256), // Random green value (0-255)
+      random.nextInt(256), // Random blue value (0-255)
+      1.0, // Opacity (fully opaque)
+    );
+  }
+
   Brightness brightness = MediaQuery.of(context).platformBrightness;
-  Color seedColor = (brightness == Brightness.dark) ? Colors.green : Colors.red;
+  Color seedColor =
+      (brightness == Brightness.dark) ? getRandomColor() : getRandomColor();
   return ThemeData(
     useMaterial3: true, // Enable Material 3 (You)
     // Color scheme that adapts to system preferences (light/dark mode)
@@ -11,8 +23,12 @@ ThemeData buildAppTheme(BuildContext context) {
       brightness: brightness,
     ),
     cardColor: ColorScheme.fromSeed(seedColor: seedColor).onPrimaryContainer,
-    appBarTheme: const AppBarTheme(
-      titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    appBarTheme: AppBarTheme(
+      titleTextStyle: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: brightness == Brightness.dark ? Colors.white : Colors.black,
+      ),
     ),
   );
 }
